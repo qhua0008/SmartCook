2036e04e1fc685c1b89f8de0c194ae05fd2a8db1
Aditi Mukhopadhyay <amuk0002@>
2018-11-01 03:22:51 +1100

    smart cook

---
+++ SmartCook/TabBarController.swift
@@ -0,0 +1,44 @@
+//
+//  TabBarController.swift
+//  SmartCook
+//
+//  Created by Aditi on 01/11/18.
+//  Copyright © 2018 Aditi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch {}
+        navigationController?.popViewController(animated: true)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
